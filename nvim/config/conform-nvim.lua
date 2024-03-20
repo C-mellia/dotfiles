55bb8feff2dc73621736e0bd8cd3da868a735c1a
C-mellia <m13418875018@gmail.com>
2024-03-20 16:40:42 +0800

    modified:   nvim/config/conform-nvim.lua 	modified:   script/tmux-sessionizer

--- nvim/config/conform-nvim.lua
+++ nvim/config/conform-nvim.lua
@@ -1,4 +1,5 @@
-require("conform").setup({
+local conform = require("conform")
+conform.setup({
 	format_on_save = {
 		-- These options will be passed to conform.format()
 		timeout_ms = 500,
@@ -7,17 +8,21 @@ require("conform").setup({
 	formatters_by_ft = {
 		css = { { "prettierd", "prettier" } },
 		lua = { "stylua" },
-		html = { "html-beautify" },
+		html = { "htmlbeautifier" },
 		-- Conform will run multiple formatters sequentially
 		python = { "isort", "black" },
 		-- Use a sub-list to run only the first available formatter
 		javascript = { { "prettierd", "prettier" } },
+		json = { { "prettierd", "prettier" } },
+		sql = { "sqlfmt" },
+		sh = { "beautysh" },
+		zsh = { "beautysh" },
 	},
 })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
 	pattern = "*",
 	callback = function(args)
-		require("conform").format({ bufnr = args.buf })
+		conform.format({ bufnr = args.buf })
 	end,
 })
