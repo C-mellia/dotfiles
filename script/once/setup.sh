a41a31f041b52c07af9cb9130bcde6a01347d946
C-mellia <m13418875018@gmail.com>
2024-03-23 17:36:47 +0800

    script

--- script/once/setup.sh
+++ script/once/setup.sh
@@ -6,20 +6,52 @@ else
     DOTFILES_DIR=$1
 fi
 
-if [[ $(pacman) 2&> /dev/null ]]; then
-    echo "Installing packages..."
-    sudo pacman -S --noconfirm --needed $(cat $DOTFILES_DIR/packages)
-else
+if [[ $(pacman 2&> /dev/null) ]]; then
     echo "Unsupported OS"
     exit 1
+else
+    echo "Installing packages..."
+    sudo pacman -S --noconfirm --needed $(cat $DOTFILES_DIR/packages)
+    if [[ ! -f /usr/bin/yay ]]; then
+        echo "Installing yay..."
+        git clone https://aur.archlinux.org/yay.git
+        cd yay
+        makepkg -si
+        cd ..
+        rm -rf yay
+    fi
 fi
 
-sudo chsh -s $(which zsh) $USER
+
+echo "Cloning tmux plugin manager..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo "Cloning Neovim Repo"
+git clone https://github.com/neovim/neovim $HOME/softwares/neovim
+
+echo "Building Neovim"
+cd $HOME/softwares/neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+sudo make install
 
 cd $DOTFILES_DIR
 
 echo "Updating submodules..."
 git submodules update --init
+
+sudo chsh -s $(which zsh) $USER
+
+echo "Installing rustup"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "Installing rust packages..."
+cargo install starship bat ripgrep fd-find
+
+echo "Installing neovim plugins..."
+nvim +PlugInstall +qall
+
+echo "Installing tmux plugins..."
+~/.tmux/plugins/tpm/bin/install_plugins
 
 echo "Linking dotfiles..."
 ln -s $DOTFILES_DIR/nvim $HOME/.config/nvim
@@ -34,9 +66,6 @@ ln -s $DOTFILES_DIR/starship.toml $HOME/.config/starship.toml
 echo "Installing fonts..."
 bash $DOTFILES_DIR/font
 
-echo "Cloning tmux plugin manager..."
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "source ${DOTFILES_DIR}/zsh" >> ~/.zshrc
-
 
 echo "All set"
