776796c2db1b6784479662406e481396018b4de4
C-mellia <m13418875018@gmail.com>
2024-03-23 17:05:11 +0800

    feat: script

---
+++ script/once/setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    DOTFILES_DIR=$HOME/dotfiles/
+else
+    DOTFILES_DIR=$1
+fi
+
+cd $DOTFILES_DIR
+git submodules update --init
+
+echo "Linking dotfiles..."
+ln -s $DOTFILES_DIR/nvim $HOME/.config/nvim
+ln -s $DOTFILES_DIR/.tmux.conf $HOME/.tmux.conf
+ln -s $DOTFILES_DIR/alacritty $HOME/.config/alacritty
+sudo ln -s $DOTFILES_DIR/i3status.conf /etc/i3status.conf
+ln -s $DOTFILES_DIR/.wezterm.lua $HOME/.wezterm.lua
+ln -s $DOTFILES_DIR/i3 $HOME/.config/i3
+ln -s $DOTFILES_DIR/newsboat $HOME/.config/newsboat
+ln -s $DOTFILES_DIR/starship.toml $HOME/.config/starship.toml
+
+echo "Installing fonts..."
+bash $DOTFILES_DIR/font
+
+echo "Cloning tmux plugin manager..."
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+echo "source ${DOTFILES_DIR}/zsh" >> ~/.zshrc
+
+
+echo "All set"
