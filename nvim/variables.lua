0463a6a986a4c6a714c86428499422c36f4a4379
C-mellia <m13418875018@gmail.com>
2024-03-31 16:02:29 +0800

    smol update

--- nvim/variables.lua
+++ nvim/variables.lua
@@ -41,7 +41,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
--- vim.opt.expandtab = true
+vim.opt.expandtab = true
 -- vim.opt.cinoptions = "(0,ws,Ws,:0,Ls,l1,p0,t0,m1,j1,g0"
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
