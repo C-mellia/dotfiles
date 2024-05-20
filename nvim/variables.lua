fb0d4ad0da3c9d0c5ad6f5b2edc726516c385512
C-mellia <m13418875018@gmail.com>
2024-05-20 20:53:28 +0800

    modified:   nvim/variables.lua 	modified:   script/template/build_util

--- nvim/variables.lua
+++ nvim/variables.lua
@@ -4,6 +4,9 @@ vim.g.maplocalleader = " "
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+vim.opt.path:append("/usr/local/include")
+vim.opt.path:append("/opt/cuda/include")
+
 vim.opt.indentkeys:append(">")
 vim.opt.guicursor = "i:block"
 vim.opt.showtabline = 0
@@ -41,7 +44,7 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 vim.opt.colorcolumn = "80"
 
--- vim.opt.expandtab = true
+vim.opt.expandtab = true
 -- vim.opt.cinoptions = "(0,ws,Ws,:0,Ls,l1,p0,t0,m1,j1,g0"
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
