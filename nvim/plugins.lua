992b6c9798a2c4521cd663227291683fbd873b89
C-mellia <m13418875018@gmail.com>
2024-12-22 14:48:49 +0800

    modified:   nvim/plugins.lua 	modified:   nvim/variables.lua

--- nvim/plugins.lua
+++ nvim/plugins.lua
@@ -121,5 +121,17 @@ require("lazy").setup({
 	--         vim.g.molten_image_provider = "image.nvim"
 	--         vim.g.molten_output_win_max_height = 20
 	--     end,
-	-- },
+
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+			{ "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+		},
+		build = "make tiktoken", -- Only on MacOS or Linux
+		opts = {
+			-- See Configuration section for options
+		},
+		-- See Commands section for default commands if you want to lazy load on them
+	}, -- },
 }, {})
--- nvim/variables.lua
+++ nvim/variables.lua
@@ -61,3 +61,5 @@ vim.g.tex_conceal = "abdmg"
 vim.opt.formatoptions:append("mB")
 vim.opt.background = "dark"
 -- vim.opt.background = 'light'
+
+vim.g.copilot_proxy = "http://localhost:20171"
